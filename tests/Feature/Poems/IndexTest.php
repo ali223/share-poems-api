f6d920f9628a68047641551faeeb8c9a3660c9e6
Mohammed Ali Essa <ma.essa.pk@gmail.com>
2020-07-14 22:03:52 +0100

    Fix the failing test

--- tests/Feature/Poems/IndexTest.php
+++ tests/Feature/Poems/IndexTest.php
@@ -19,23 +19,23 @@ class IndexTest extends TestCase
             ->seeJson([
                 'id' => $poems[0]->id,
                 'title' => $poems[0]->title,
-                'poet_name' => $poems[0]->poet_name,
+                'poetName' => $poems[0]->poet_name,
                 'content' => $poems[0]->content,
-                'image_url' => $poems[0]->image_url,
+                'imageUrl' => $poems[0]->image_url,
             ])
             ->seeJson([
                 'id' => $poems[1]->id,
                 'title' => $poems[1]->title,
-                'poet_name' => $poems[1]->poet_name,
+                'poetName' => $poems[1]->poet_name,
                 'content' => $poems[1]->content,
-                'image_url' => $poems[1]->image_url,
+                'imageUrl' => $poems[1]->image_url,
             ])
             ->seeJson([
                 'id' => $poems[2]->id,
                 'title' => $poems[2]->title,
-                'poet_name' => $poems[2]->poet_name,
+                'poetName' => $poems[2]->poet_name,
                 'content' => $poems[2]->content,
-                'image_url' => $poems[2]->image_url,
+                'imageUrl' => $poems[2]->image_url,
             ]);
     }
 }
