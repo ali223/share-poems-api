fa4f0f48b6e95cd0b0380695658993b22898372f
Mohammed Ali Essa <ma.essa.pk@gmail.com>
2020-08-24 16:12:45 +0100

    Install the laravel tinker package

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -97,6 +97,7 @@ $app->register(App\Providers\AuthServiceProvider::class);
 // $app->register(App\Providers\EventServiceProvider::class);
 $app->register(Fruitcake\Cors\CorsServiceProvider::class);
 $app->register(Tymon\JWTAuth\Providers\LumenServiceProvider::class);
+$app->register(Laravel\Tinker\TinkerServiceProvider::class);
 
 /*
 |--------------------------------------------------------------------------
